d739e4ff11fa05c8bede15e5edb538ff5a5685b1
RastyDen <70960711+RastyDen@users.noreply.github.com>
2020-12-21 19:31:20 +0300

    Add files via upload

---
+++ ind1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+sentence = input("Введите предложение: ")
+for i in sentence:
+   if i in {"м", "н"}:
+    print(i, end='')
+
+
+
+
+
+
---
+++ ind3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    s = input("Введите предложение: ")
+    t = input("Введите букву: ")
+    r = s.replace('и', 'и'+t, 1)
+    print("Предложение после редактирования:", r)
+
