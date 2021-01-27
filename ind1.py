b4b146e86a6ec63abe94f66a40dc0b97030b684c
RastyDen <70960711+RastyDen@users.noreply.github.com>
2021-01-27 11:58:24 +0300

    Update ind1.py

--- ind1.py
+++ ind1.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-sentence = input("Введите предложение: ")
-for i in sentence:
-   if i in {"м", "н"}:
-    print(i, end='')
+
+if __name__ == '__main__':
+   sentence = input("Введите предложение: ")
+   for i in sentence:
+         if i in {"м", "н"}:
+            print(i, end='')
 
 
 
