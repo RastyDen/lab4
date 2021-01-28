845f03f99d02b5c02c786e58c2b2f10c77b6f984
RastyDen <70960711+RastyDen@users.noreply.github.com>
2021-01-28 13:43:11 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import sys
+
 if __name__ == '__main__':
     s = input("Введите предложение: ")
     n = int(input("Введите длину: "))
+    
     # Проверить требуюемую длину.
     if len(s) >= n:
         print(
@@ -11,6 +14,7 @@ if __name__ == '__main__':
             file=sys.stderr
         )
         exit(1)
+        
     # Разделить предложение на слова.
     words = s.split(' ')
     # Проверить количество слов в предложении.
@@ -20,17 +24,22 @@ if __name__ == '__main__':
             file=sys.stderr
         )
         exit(1)
+        
     # Количество пробелов для добавления.
     delta = n
     for word in words:
         delta -= len(word)
+        
     # Количество пробелов на каждое слово.
     w, r = delta // (len(words) - 1), delta % (len(words) - 1)
+    
     # Сформировать список для хранения слов и пробелов.
     lst = []
+    
     # Пронумеровать все слова в списке и перебрать их.
     for i, word in enumerate(words):
         lst.append(word)
+        
         # Если слово не является последним, добавить пробелы.
         if i < len(words) - 1:
             # Определить количество пробелов.
@@ -41,5 +50,6 @@ if __name__ == '__main__':
             # Добавить заданное количество пробелов в список.
             if width > 0:
                 lst.append(' ' * width)
+    
     # Вывести новое предложение, объединив все элементы списка lst.
     print(''.join(lst))
