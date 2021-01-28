7c5c0dd39aa6dffb5b3737c1b1975dbebea57571
RastyDen <70960711+RastyDen@users.noreply.github.com>
2021-01-28 11:27:06 +0300

    Delete ind3.py

--- ind3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-if __name__ == '__main__':
-    s = input("Введите предложение: ")
-    t = input("Введите букву: ")
-    r = s.replace('и', 'и'+t, 1)
-    print("Предложение после редактирования:", r)
-
